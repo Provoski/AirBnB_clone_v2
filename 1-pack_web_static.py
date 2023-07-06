bc86a3e2c0b2379a312f1e4683b877d05339d231
Superstazdare <superstaz.dare@gmail.com>
2023-07-06 14:42:06 -0700

    update

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,7 +9,7 @@ def do_pack():
     """
 
     d = datetime.now()
-    now = d.strftime('%Y%m%d%H%M%S')
+    now = d.strftime('%Y%m%d%h%m%s')
 
     local("mkdir -p versions")
     local("tar -czvf versions/web_static_{}.tgz web_static".format(now))
